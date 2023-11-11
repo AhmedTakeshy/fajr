2d6464e602e8e395857237ce226691cb177dbc61
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-11-11 17:31:34 +0300

    Add remote image patterns and new package react-multi-carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import CarouselItem from '@/components/CarouselItem'
 import { ModeToggler } from '@/components/ModeToggler'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
+    <div >
       <h1>أهلا وسهلا</h1>
-      <ModeToggler />
+      {/* <CarouselItem /> */}
     </div>
   )
 }
--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -8,7 +8,6 @@ import {
 } from "@/components/ui/navigation-menu"
 import Link from "next/link"
 import { ModeToggler } from "./ModeToggler"
-import { buttonVariants, Button } from "./ui/button"
 import Image from "next/image"
 import { useState } from "react"
 
@@ -17,54 +16,33 @@ export default function NavMenu() {
     const [open, setOpen] = useState<boolean>(false)
 
     return (
-        <div>
-
-            <div className="flex flex-col items-center mx-auto md:flex-row">
-                <NavigationMenu>
-                    <NavigationMenuList className="flex flex-col items-center justify-center gap-2">
-                        <NavigationMenuItem>
-                            <Link href="/" legacyBehavior passHref>
-                                <NavigationMenuLink onClick={() => setOpen(false)} className={navigationMenuTriggerStyle()}>Home</NavigationMenuLink>
-                            </Link>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <Link href="/posts" legacyBehavior passHref>
-                                <NavigationMenuLink onClick={() => setOpen(false)} className={navigationMenuTriggerStyle()}>Posts</NavigationMenuLink>
-                            </Link>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <Link href="/profile" legacyBehavior passHref>
-                                <NavigationMenuLink onClick={() => setOpen(false)} className={navigationMenuTriggerStyle()}>Profile</NavigationMenuLink>
-                            </Link>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu>
-                <div className="flex flex-col w-full md:w-full md:hidden">
-                    <div className="flex flex-col items-center justify-center gap-3 p-4 mt-4 font-medium rounded-lg md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:dark:bg-gray-900">
-                        <ModeToggler className="hidden md:inline-block" />
-                    </div>
-                </div>
-            </div>
-            <NavigationMenu>
-                <NavigationMenuList className="items-center justify-center hidden gap-2 md:flex">
-                    <NavigationMenuItem>
-                        <Link href="/" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Home</NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Link href="/posts" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Posts</NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Link href="/profile" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Profile</NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-        </div>
-
+        <NavigationMenu dir="rtl" className="items-center justify-between hidden gap-2 md:flex my-8 !container sticky top-4 w-full">
+            <Image className="ml-auto" src="https://okab.pixeldima.com/business-rtl/wp-content/uploads/sites/6/2021/01/okab-ar.png" width={130} height={32} alt="logo" />
+            <NavigationMenuList className="items-center justify-between hidden gap-2 md:flex ">
+                <NavigationMenuItem>
+                    <Link href="/" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>الرئيسية</NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="#about" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>من نحن</NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="#contact" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>اتصل بنا</NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="#services" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>الخدمات</NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <ModeToggler />
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
     )
 }
