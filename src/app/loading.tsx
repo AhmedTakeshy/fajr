2f4d128ee2ae41ced64cfbe891f8294d6c223381
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-11-18 17:37:31 +0300

    Add error and not-found components

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,6 +3,8 @@ import { ImSpinner9 } from 'react-icons/im'
 
 export default function loading() {
   return (
-      <ImSpinner9 className="ease-in-out animate-spin" size={25} />
+      <div className='flex items-center justify-center w-screen h-[calc(100vh-13.68rem)]'>
+      <ImSpinner9 className="ease-in-out animate-spin" size={100} />
+      </div>
   )
 }
