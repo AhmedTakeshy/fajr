838bb6ac5a9536a72c15479276ec33ceaa045690
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-11-20 10:23:21 +0300

    Fix type from developer by to developed by

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer({ font }: FooterProps) {
   return (
     <footer className='flex flex-col items-center justify-center w-full mt-auto bg-gray-300 pt-9 dark:bg-sky-950'>
         <p className=' text-slate-800 dark:text-slate-100'>جميع الحقوق محفوظة Fajr © 2023</p>
-          <p className={`my-4 ${font.className} `}>Developer by {" "}
+          <p className={`my-4 ${font.className} `}>Developed by {" "}
               <Link href='https://www.takeshy.works/' target='_blank' className="font-bold transition-all duration-500 text-rose-600 hover:tracking-widest hover:will-change-transform">Takeshy</Link>
         </p>
     </footer>
