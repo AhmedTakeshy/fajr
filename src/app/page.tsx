5bf3e9f62030b9fca453246509454076a469fcb3
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-11-20 10:22:40 +0300

    Fix the top of the carousel and add smooth scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Team from '@/components/Team';
 export default function Home() {
 
   return (
-    <main className="relative top-[-6.5rem]">
+    <main className="relative top-[-6.5rem] md:-top-10">
       <CarouselItem />
       <Services />
       <Jobs />
