8cd61df3acfa6fb9b7590bdb5face11dec64a095
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-11-29 12:54:45 +0300

    Update admin page and tailwind config

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import Link from 'next/link'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { getPosts } from '@/lib/actions'
 
-export default function Admin() {
+
+export default async function Admin() {
+  const posts = await getPosts()
   return (
-    <div>Admin</div>
+    <div className="!container grid sm:grid-cols-2 place-items-center gap-8 mt-12 text-3xl font-extrabold ">
+      <Button size="lg" className='w-2/4 h-12 p-1 animate-animateBorder rounded-xl bg-gradient-to-tr from-[#00FFA3] to-[#DC1FFF] bg-[length:_400%_400%]'>
+        <Link href="/posts" className={`bg-slate-900 w-full h-full rounded-lg text-gray-200 inline-flex items-center justify-center `}>منشورات</Link>
+        
+      </Button>
+      <Button size="lg" className='w-2/4 h-12 p-1 rounded-xl bg-gradient-to-r to-[#00FFA3] from-[#DC1FFF] bg-[length:_400%_400%]'>
+        <Link href="/accounts" className={`bg-slate-900 w-full h-full rounded-lg text-gray-200 inline-flex items-center justify-center`}>حسابات</Link>
+      </Button>
+    </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   theme: {
     container: {
       center: true,
@@ -65,19 +65,18 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
-        "fade-in-out": {
-          "0": { opacity: 0 },
-          "25": { opacity: 1 },
-          "75": { opacity: 1 },
-          "100": { opacity: 0 },
+        "animateBorder": {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%" :{ backgroundPosition: "100% 50%" },
+          "100%" :{ backgroundPosition: "0% 50%" }
+        }
 
-        },
-        
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "fade-in-out": "fade-in-out 2s ease-in-out infinite forwards",
+        "animateBorder": "animateBorder 3s ease infinite"
       },
     },
   },
