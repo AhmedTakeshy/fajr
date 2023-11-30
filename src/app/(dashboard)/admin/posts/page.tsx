2cebc732a385e0fdac65848cd4446591e6f019d0
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-11-30 13:09:57 +0300

    Update JobItem import and display posts dynamically

--- src/app/(dashboard)/admin/posts/page.tsx
+++ src/app/(dashboard)/admin/posts/page.tsx
@@ -1,12 +1,15 @@
-import JobItem from '@/components/main/JobItem'
+import JobItem from '@/components/main/PostItem'
 import { Button } from '@/components/ui/button'
 import { getPosts } from '@/lib/actions'
+import { PostFormSchema } from '@/lib/formSchemas'
 import Link from 'next/link'
 import { FiEdit } from 'react-icons/fi'
 
 
+
+
 export default async function Posts() {
-    // const posts = await getPosts()
+    const posts = await getPosts()
     return (
         <div className="container grid gap-8 my-12 text-3xl font-extrabold place-items-start ">
             <Button size="lg" asChild variant="outline">
@@ -14,7 +17,9 @@ export default async function Posts() {
                     اكتب مقال <FiEdit size={20} className="mr-2" /></Link> 
             </Button>
         <div className="grid gap-10 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 place-items-center mt-8">
-            <JobItem id={1} title="مطور ويب" description="الآن، كان أبحرت من نانتوكيت في البداية من على خط الموسم على اساس. لا المسعى ممكن داخل المستودع." img="/images/cat-01.jpg" />
+            {posts.map((post) => (
+                <JobItem key={post.id} id={post.id} title={post.title} description={post.content!} />
+            ))}
         </div>
         </div>
     )
