accf33e21760e0a542e9282ee2af287201756be2
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-12-02 16:29:13 +0300

    Update package-lock.json and types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,12 +1,12 @@
 type Post = {
-    id: bigint;
+    id: number;
     publicId: string;
     title: string;
     topic: string | null;
     content: string;
     image: string | null;
     published: boolean;
-    authorId: bigint;
+    authorId: number;
     createdAt: Date;
     updatedAt: Date;
 }
