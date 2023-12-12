9ba8c0bbcc2b48442fd05ecb78e16d85289fdd8b
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-12-12 14:32:37 +0300

    Refactor Map component and add location marker

--- src/components/main/Map.tsx
+++ src/components/main/Map.tsx
@@ -1,35 +1,43 @@
 "use client"
-import Map, { Marker, Popup, NavigationControl, GeolocateControl, FullscreenControl } from "react-map-gl";
+import Map, { Marker, Popup, NavigationControl, FullscreenControl } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useState } from "react";
 import Link from "next/link";
+import { ImLocation } from "react-icons/im";
 
 export default function MapPage() {
     const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
     const [showPopup, setShowPopup] = useState<boolean>(true);
-    const viewState = {
+    const [viewState, setViewState] = useState({
         latitude: 33.30430739551274,
         longitude: 44.323928150187406,
-        zoom: 12,
-    }
+        zoom: 16,
+    })
     return (
         <div className="max-w-full mx-8 my-4">
             <Map
                 mapboxAccessToken={mapboxToken}
                 mapStyle="mapbox://styles/mapbox/streets-v12"
                 {...viewState}
-                style={{ width: "100%", height: "100%", borderRadius: "8px" }}
+                style={{ width: 600, height: 450, borderRadius: "1rem" }}
+                cursor="pointer"
+                optimizeForTerrain={true}
+                onZoom={(e) => setViewState(e.viewState)}
             >
-                <Link href=" https://maps.app.goo.gl/fFC5S6ba6ABJiBuz6" target="_blank">
-                <Marker longitude={viewState.longitude} latitude={viewState.latitude} color="red" anchor="top"/>
-                    {showPopup && (
-                        <Popup style={{ color: "black", fontWeight: "700", fontSize: "1.1rem" }} longitude={viewState.longitude} latitude={viewState.latitude}
-                            offset={25}
-                            anchor="top"
-                            onClose={() => setShowPopup(false)}>
-                            شركة فجر بغداد
-                        </Popup>)}
-                </Link>
+                <Marker longitude={viewState.longitude} latitude={viewState.latitude} color="red" anchor="top">
+                    <Link href="https://maps.app.goo.gl/fFC5S6ba6ABJiBuz6" target="_blank">
+                        <ImLocation size={30} className="text-red-500" />
+                    </Link>
+                </Marker>
+                <ImLocation size={30} className="text-red-500" />
+                {showPopup && (
+                    <Popup style={{ color: "black", fontWeight: "700", fontSize: "1.1rem", width: "8rem", padding: ".7rem", textAlign: "center" }} longitude={viewState.longitude} latitude={viewState.latitude}
+                        offset={20}
+                        anchor="top"
+                        onClose={() => setShowPopup(false)}
+                    >
+                        شركة فجر بغداد
+                    </Popup>)}
                 <FullscreenControl position="top-left" />
                 <NavigationControl position="top-left" />
             </Map>
