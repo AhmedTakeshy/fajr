1cd887724ed9b49fa462626a2f8d49df1bf2c7ad
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-12-12 12:44:28 +0300

    Update styles and remove unused code

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,10 @@
 import CarouselItem from '@/components/main/CarouselItem'
 import Posts from '@/components/main/Posts';
 import Services from '@/components/main/Services';
-import Team from '@/components/main/Team';
 export default function Home() {
 
   return (
-    <main className="relative top-[-11rem] md:-top-28 flex-grow">
+    <main className="relative top-[-11rem] md:top-[-4.5rem] flex-grow">
       <CarouselItem />
       <Services />
       <Posts />
