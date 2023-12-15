5e48ae820a8d92fa20621b8ca6e65b354f76c420
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-12-15 14:50:57 +0300

    Refactor Services component to use objects for services

--- src/components/main/Services.tsx
+++ src/components/main/Services.tsx
@@ -5,9 +5,18 @@ import ServiceItem from "./ServiceItem"
 
 export default function Services() {
 
-  const services1 = ["الآن، كان أبحرت من نانتوكيت في البداية من على خط الموسم على اساس. لا المسعى ممكن داخل المستودع."]
-  const services2 = ["الآن، كان أبحرت من نانتوكيت في البداية من على خط الموسم على اساس. لا المسعى ممكن داخل المستودع."]
-  const services3 = ["الآن، كان أبحرت من نانتوكيت في البداية من على خط الموسم على اساس. لا المسعى ممكن داخل المستودع."]
+  const services1 = {
+    title: "الخدمات التي توفرها الشركة",
+    description: ["الآن، كان أبحرت من نانتوكيت في البداية من على خط الموسم على اساس. لا المسعى ممكن داخل المستودع."],
+  }
+  const services2 = {
+    title: "وسائل التواصل الاجتماعي",
+    description: ["الآن، كان أبحرت من نانتوكيت في البداية من على خط الموسم على اساس. لا المسعى ممكن داخل المستودع."],
+  }
+  const services3 = {
+    title: "وسائل التواصل الاجتماعي",
+    description: ["الآن، كان أبحرت من نانتوكيت في البداية من على خط الموسم على اساس. لا المسعى ممكن داخل المستودع."],
+  }
 
   return (
     <MotionSection 
@@ -19,18 +28,18 @@ export default function Services() {
       <h1 className='my-6 font-bold text-7xl'>خدماتنا</h1>
       <section id="services" className='grid gap-8 my-4 lg:grid-cols-3 md:grid-cols-2 place-content-center'>
         <ServiceItem
-          description={services1}
-          title="الخدمات التي توفرها الشركة"
+          description={services1.description}
+          title={services1.title}
           icon={<HiOutlineSpeakerphone size={40} className="text-cyan-400" />}
         />
         <ServiceItem
-          description={services2}
-          title='وسائل التواصل الاجتماعي'
+          description={services2.description}
+          title={services2.title}
           icon={<HiOutlineSpeakerphone size={40} className="text-cyan-400" />}
         />
         <ServiceItem
-          description={services3}
-          title='وسائل التواصل الاجتماعي'
+          description={services3.description}
+          title={services3.title}
           icon={<HiOutlineSpeakerphone size={40} className="text-cyan-400" />}
         />
       </section>
