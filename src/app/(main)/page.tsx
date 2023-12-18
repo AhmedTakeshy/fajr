717bd524f3e4f19f56d214fbd535c3356216e6f8
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-12-18 14:39:03 +0300

    Refactor page component and add pagination controls

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,10 @@
 import CarouselItem from '@/components/main/CarouselItem'
 import Posts from '@/components/main/Posts';
 import Services from '@/components/main/Services';
-export default function Home() {
 
+
+
+export default function Home() {
   return (
     <main className="relative top-[-10.1rem] md:top-[-4.7rem] flex-grow">
       <CarouselItem />
--- src/app/(main)/posts/page.tsx
+++ src/app/(main)/posts/page.tsx
@@ -1,21 +1,32 @@
 import { getPosts } from "@/_actions/postActions";
+import PaginationControls from "@/components/PaginationControls";
 import PostItem from "@/components/PostItem";
 
+type Props = {
+    searchParams: { [key: string]: string | string[] | undefined }
+}
 
-export default async function page() {
-    const posts = await getPosts({ page: 1 });
+export default async function page(props: Props) {
+    const searchParams = props.searchParams;
+    const page = searchParams.page;
+    const { posts, metadata } = await getPosts({ pageNumber: parseInt(page as string) });
+
+
     return (
-        <div className="container grid gap-10 mx-auto my-20 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 place-items-center place-content-center">
-            {posts.map(post => !!post.published &&
-            <PostItem 
-            key={post.id} 
-            id={post.id} 
-            publicId={post.publicId} 
-            title={post.title}
-            topic={post.topic || ""}
-            description={post.content} 
-            updatedAt={post.updatedAt} 
-            />)}
-        </div>
+        <section className="container flex flex-col items-center justify-start">
+            <div className="grid gap-10 mx-auto my-20 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 place-items-start place-content-center">
+                {posts.map(post => !!post.published &&
+                    <PostItem
+                        key={post.id}
+                        id={post.id}
+                        publicId={post.publicId}
+                        title={post.title}
+                        topic={post.topic || ""}
+                        description={post.content}
+                        updatedAt={post.updatedAt}
+                    />)}
+            </div>
+            <PaginationControls metadata={metadata} pageNumber={page as string} />
+        </section>
     )
 }
