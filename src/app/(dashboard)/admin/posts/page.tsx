82420be72d78fd8096559dc9ff540016c6306426
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-12-19 12:10:02 +0300

    Refactor Posts component to include pagination controls

--- src/app/(dashboard)/admin/posts/page.tsx
+++ src/app/(dashboard)/admin/posts/page.tsx
@@ -8,18 +8,18 @@ type Props = {
 }
 
 export default async function Posts({ searchParams }: Props) {
-    console.log("🚀 ~ file: page.tsx:11 ~ Posts ~ searchParams:", searchParams)
-    const { posts } = await getPosts({ pageNumber: 1 })
-    
+    const page = searchParams.page as string || "1";
+    const { posts, metadata } = await getPosts({ pageNumber: parseInt(page), url: "/admin/posts" });
+
     return (
-    <div className="container grid gap-8 my-20 text-3xl font-extrabold place-items-start ">
+        <div className="container grid gap-8 my-20 text-3xl font-extrabold place-items-start ">
             <PostForm type='new' />
-        <div className="grid gap-10 mt-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 place-items-start">
-            {posts.map((post) => (
-                <PostItem updatedAt={post.updatedAt} key={post.id} publicId={post.publicId} id={post.id} title={post.title} description={post.content!} topic={post.topic || ""}/>
-            ))}
+            <div className="grid gap-10 mt-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 place-items-start">
+                    {posts.map((post) => (
+                        <PostItem updatedAt={post.updatedAt} key={post.id} publicId={post.publicId} id={post.id} title={post.title} description={post.content!} topic={post.topic || ""} />
+                    ))}
+            </div>
+            <PaginationControls metadata={metadata} pageNumber={page} />
         </div>
-        {/* <PaginationControls /> */}
-    </div>
     )
 }
