ccdf92644e4769ffaaa5fcc3cbe4d8e54fd2a4de
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2023-12-20 01:42:18 +0300

    Refactor post rendering in Admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -15,7 +15,18 @@ export default async function Admin({ searchParams }: Props) {
       <div className="!container flex flex-col items-center my-20 text-3xl font-extrabold gap-20">
         <PostForm type='new' />
         <div className="container grid gap-10 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 place-items-start">
-          {posts.map(post => <PostItem key={post.id} id={post.id} publicId={post.publicId} title={post.title} description={post.content} updatedAt={post.updatedAt} />)}
+          {posts
+            .map(post =>
+              <PostItem
+                key={post.id}
+                id={post.id}
+                publicId={post.publicId}
+                title={post.title}
+                description={post.content}
+                updatedAt={post.updatedAt}
+              />)
+            .slice(0, 8)
+          }
         </div>
         <Button asChild variant="link" className="self-start text-xl ">
           <Link href={{ pathname: "/admin/posts", query: { page: "1" } }}>المزيد من الوظائف</Link>
