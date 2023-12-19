9bd8b01454ad67215d9e20066f8aacb84779da46
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-12-19 12:10:24 +0300

    Add logo image to loading screen

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,12 @@
 // "use client"
+import Image from 'next/image'
 import { ImSpinner9 } from 'react-icons/im'
 
-export default function loading() {
+export default function Loading() {
   return (
       <div className='flex items-center justify-center w-screen h-screen'>
-      <ImSpinner9 className="ease-in-out animate-spin" size={100} />
+      {/* <ImSpinner9 className="ease-in-out animate-spin" size={100} /> */}
+      <Image className="w-56 ease-in-out animate-pulse" src="/imgs/logo.png" width={160} height={75} alt="logo" />
       </div>
   )
 }
