0c465838d85584a6786ed568e91bec6b3cd1975e
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-12-20 11:45:08 +0300

    Update Testimonials component styling

--- src/components/main/Testimonials.tsx
+++ src/components/main/Testimonials.tsx
@@ -17,10 +17,10 @@ export default function Testimonials() {
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: true, amount: 0.7 }}
-            className='bg-[url("/imgs/pic1-big.jpg")] relative z-[3] mt-24  bg-center'>
+            className='bg-[url("/imgs/pic1-big.jpg")] relative z-[3] mt-24 bg-fixed bg-center'>
             <div className="absolute w-full h-full z-[5] bg-black/80" />
-            <h1 className="py-8 text-6xl font-bold relative text-center z-10">اراء عملائنا</h1>
-            <div className="relative z-10 grid gap-4 mx-auto max-w-[60rem] md:grid-cols-5 place-items-center p-4">
+            <h1 className="py-8 xl:text-6xl lg:text-5xl text-3xl font-bold relative text-center z-10">اراء عملائنا</h1>
+            <div className="relative z-10 grid gap-4 mx-auto max-w-[60rem] md:grid-cols-5 place-items-center p-8">
                 <Image
                     className="border border-white w-full h-full col-span-2 row-span-2 bg-transparent rounded-md md:col-span-3"
                     src={testPic4}
