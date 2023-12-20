23564143be278874bd639f1bad6facee0c8e47a0
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-12-20 11:32:34 +0300

    Refactor Testimonials component styles

--- src/components/main/Testimonials.tsx
+++ src/components/main/Testimonials.tsx
@@ -17,42 +17,53 @@ export default function Testimonials() {
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: true, amount: 0.7 }}
-            className='bg-[url("/imgs/pic1-big.jpg")] relative z-[3] h-4/6 mt-24 bg-center'>
-            <div className="absolute w-full h-full z-[5] bg-black/60" />
-            <div className="relative z-10 grid grid-rows-none gap-2 py-4 mx-auto max-w-[1240px] md:grid-cols-5 md:gap-4 place-content-center place-items-center">
+            className='bg-[url("/imgs/pic1-big.jpg")] relative z-[3] mt-24  bg-center'>
+            <div className="absolute w-full h-full z-[5] bg-black/80" />
+            <h1 className="py-8 text-6xl font-bold relative text-center z-10">اراء عملائنا</h1>
+            <div className="relative z-10 grid gap-4 mx-auto max-w-[60rem] md:grid-cols-5 place-items-center p-4">
                 <Image
-                    className="object-cover w-full h-[70%] col-span-2 row-span-2 bg-transparent rounded-md md:col-span-3"
+                    className="border border-white w-full h-full col-span-2 row-span-2 bg-transparent rounded-md md:col-span-3"
                     src={testPic4}
-                    width={725.5}
-                    height={479.5}
+                    width={725}
+                    height={479}
+                    placeholder="blur"
+                    blurDataURL={testPic4}
                     alt="testimonials image"
                 />
                 <Image
-                    className="object-cover w-full h-5/6 rounded-tl-md"
+                    className="object-cover border-2 border-white w-full h-full rounded-tl-md"
                     src={testPic1}
-                    width={228.8}
-                    height={239.067}
+                    width={228}
+                    height={239}
+                    placeholder="blur"
+                    blurDataURL={testPic1}
                     alt="testimonials image"
                 />
                 <Image
-                    className="object-cover w-full h-5/6 rounded-tr-md"
+                    className="object-cover border-2 border-white w-full h-full rounded-tr-md"
                     src={testPic3}
-                    width={228.8}
-                    height={239.067}
+                    width={228}
+                    height={239}
+                    placeholder="blur"
+                    blurDataURL={testPic3}
                     alt="testimonials image"
                 />
                 <Image
-                    className="object-cover w-full h-5/6 rounded-bl-md"
+                    className="object-cover border-2 border-white w-full h-full rounded-bl-md"
                     src={testPic6}
-                    width={228.8}
-                    height={239.067}
+                    width={228}
+                    height={239}
+                    placeholder="blur"
+                    blurDataURL={testPic6}
                     alt="testimonials image"
                 />
                 <Image
-                    className="object-cover w-full h-5/6 rounded-br-md"
+                    className="object-cover border-2 border-white w-full h-full rounded-br-md"
                     src={testPic5}
-                    width={228.8}
-                    height={239.067}
+                    width={228}
+                    height={239}
+                    placeholder="blur"
+                    blurDataURL={testPic5}
                     alt="testimonials image"
                 />
             </div>
