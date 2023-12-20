87a1862ae7efa187cf7de01f2a3646cbf4f53194
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-12-20 10:45:46 +0300

    Refactor ScrollButton component

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -1,11 +1,11 @@
 "use client"
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { IoIosArrowUp } from "react-icons/io"
 import { Button } from './ui/button'
 
 export default function ScrollButton() {
     const [showBtn, setShowBtn] = useState(false);
-    
+
     const scrollTo = () => {
         window.scrollTo({
             top: 0,
@@ -20,9 +20,9 @@ export default function ScrollButton() {
         });
     }, []);
 
-  return (
-      <Button aria-label='scroll' aria-description='scroll to top' onClick={scrollTo} size="icon" className={`border-1 ${showBtn ? "opacity-100" : "opacity-0"} duration-300 transition-all fixed z-10 flex items-center justify-center right-4 bottom-4`} >
-        <IoIosArrowUp size={25} />
-    </Button>
-  )
+    return (
+        <Button aria-label='scroll' aria-description='scroll to top' onClick={scrollTo} size="icon" className={`border-1 ${showBtn ? "opacity-100" : "opacity-0"} duration-300 transition-all fixed z-10 flex items-center justify-center right-4 bottom-4`} >
+            <IoIosArrowUp size={25} />
+        </Button>
+    )
 }
