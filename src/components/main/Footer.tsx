7613e13f524169d7954722871ff7915d97096fec
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-12-20 10:45:16 +0300

    Refactor Footer component to use dynamic imports

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -1,16 +1,12 @@
-import { NextFont } from 'next/dist/compiled/@next/font'
 import Link from 'next/link'
-import React from 'react'
-import { FaTiktok, FaTelegram, FaFacebookF, FaInstagram, FaPhoneAlt } from "react-icons/fa";
+import dynamic from 'next/dynamic';
+import { FaTiktok, FaTelegram, FaFacebookF, FaInstagram } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
-import SubscribeForm from './SubscribeForm';
 import { inter } from '@/lib/fonts';
 import Image from 'next/image';
-import { Suspense } from 'react';
-import { Skeleton } from '../ui/skeleton';
-import MapPage from './Map';
 
-
+const MapPage = dynamic(() => import('./Map'))
+const SubscribeForm = dynamic(() => import('./SubscribeForm'))
 
 export default function Footer() {
 
@@ -68,25 +64,25 @@ export default function Footer() {
           <div className='order-2 xl:order-none'>
             <p className=' text-slate-800 dark:text-slate-100'>جميع الحقوق محفوظة شـــــــركة فجـــــــر بغــــــداد © 2023</p>
             <p className={`${inter.className} !mt-0`}>تم التصميم والتطوير بواسطة
-              <Link href='https://www.takeshy.works/' target='_blank' className="font-bold transition-all duration-500 text-rose-600 hover:tracking-widest hover:will-change-transform"> Takeshy</Link>
+              <Link href='https://www.takeshy.works/' target='_blank' className="font-bold transition-all duration-500 text-cyan-500 hover:tracking-widest hover:will-change-transform"> Takeshy</Link>
             </p>
           </div>
           <div className='order-1 xl:order-none'>
             <div className='flex items-center justify-center gap-3 mb-2 xl:justify-end'>
-              <Link href="https://www.tiktok.com/@fajer_bg?_t=8hStlZjRw18&_r=1" target="_blank" className='relative group'>
+              <Link aria-label='navigate to tiktok' href="https://www.tiktok.com/@fajer_bg?_t=8hStlZjRw18&_r=1" target="_blank" className='relative group'>
                 <Image src='/imgs/tiktok.svg' width={20} height={20} className='transition duration-500 opacity-0 group-hover:opacity-100' alt='TikTok logo' />
                 <FaTiktok className="absolute top-[50%] translate-y-[-50%] right-0 transition duration-500 opacity-100 group-hover:opacity-0" size={20} />
               </Link>
-              <Link href="https://t.me/gghhtyruiopew" target="_blank" className='group'>
+              <Link aria-label='navigate to telegram channel' href="https://t.me/gghhtyruiopew" target="_blank" className='group'>
                 <FaTelegram size={20} className="group-hover:text-[#2AABEE] duration-500 transition" />
               </Link>
-              <Link href="mailto:Fajer.baghdad89@gmail.com" className='group'>
+              <Link aria-label='send email' href="mailto:Fajer.baghdad89@gmail.com" className='group'>
                 <HiMail size={25} className="transition-all duration-500 group-hover:text-rose-400" />
               </Link>
-              <Link href="https://www.facebook.com/profile.php?id=100077916062174&mibextid=ZbWKwL" target="_blank" className=' group'>
+              <Link aria-label='navigate to facebook' href="https://www.facebook.com/profile.php?id=100077916062174&mibextid=ZbWKwL" target="_blank" className=' group'>
                 <FaFacebookF size={20} className="group-hover:text-[#1877f2] duration-500 transition" />
               </Link>
-              <Link href="https://instagram.com/fjer_b?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" className='relative before:hover:bg-gradient-to-br from-[#f9ce34] via-[#ee2a7b] to-[#6228d7] before:rounded-[6px] before:w-full before:h-full before:opacity-0 before:hover:opacity-100 before:duration-1000 before:transition-all before:absolute before:z-0 '>
+              <Link aria-label='navigate to instagram' href="https://instagram.com/fjer_b?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" className='relative before:hover:bg-gradient-to-br from-[#f9ce34] via-[#ee2a7b] to-[#6228d7] before:rounded-[6px] before:w-full before:h-full before:opacity-0 before:hover:opacity-100 before:duration-1000 before:transition-all before:absolute before:z-0 '>
                 <FaInstagram size={20} className="relative z-[1]" />
               </Link>
             </div>
