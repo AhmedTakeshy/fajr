42344b9de178c9fea6d87aaf488c11c36a5cb8d4
Ahmed Takeshy <ahmedtakeshy7@gmail.com>
2023-12-23 14:42:08 +0300

    Remove unused imports and commented code in loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,8 @@
-// "use client"
 import Image from 'next/image'
-import { ImSpinner9 } from 'react-icons/im'
 
 export default function Loading() {
   return (
       <div className='flex items-center justify-center w-screen h-screen'>
-      {/* <ImSpinner9 className="ease-in-out animate-spin" size={100} /> */}
       <Image className="w-56 ease-in-out animate-pulse" src="/imgs/logo.png" width={160} height={75} alt="logo" />
       </div>
   )
