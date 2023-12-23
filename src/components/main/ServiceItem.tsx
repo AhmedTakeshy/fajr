fa6cb3ddf13f5e411b69eb5371e6889cd840f3a1
Ahmed Takeshy <ahmedtakeshy7@gmail.com>
2023-12-23 14:42:31 +0300

    Update ServiceItem component styles

--- src/components/main/ServiceItem.tsx
+++ src/components/main/ServiceItem.tsx
@@ -8,10 +8,10 @@ type ServiceProps = {
 
 export default function ServiceItem({ title, description, icon }: ServiceProps) {
     return (
-        <article  className='grid grid-cols-[1fr,auto] place-items-start text-right gap-x-4 place-content-start'>
+        <article  className='grid sm:grid-cols-[1fr,auto] place-items-start text-right gap-x-4 place-content-start sm:border-none border-2 rounded-xl p-2 sm:p-0'>
             <h3 className="mb-2 text-2xl font-semibold text-black dark:text-slate-100">{title}</h3>
             {icon}
-            <ul className="space-y-2 text-xl list-disc dark:text-slate-300 text-slate-700">
+            <ul className="space-y-2 text-xl sm:list-disc dark:text-slate-300 text-slate-700">
                 {description.map((item, index) => (
                     <li className="" key={index}>{item}</li>
                 ))}
